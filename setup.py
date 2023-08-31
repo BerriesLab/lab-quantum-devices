1c85fe7e5b0614a7a11427f0a4629c714e43b9d4
dabe270587 <Db270587!>
2023-08-31 11:17:46 +0200

    Add folder and moved files around

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='optical_image',
+    name='Lab-Scripts',
     version='1',
     author='Davide Beretta',
     author_email='mail.davide.beretta@gmail.com',
