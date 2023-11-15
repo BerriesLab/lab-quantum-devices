b110c3354b0f869068646dd371c5436417e0797e
dabe270587 <Db270587!>
2023-11-15 18:52:17 +0100

    Add folder and moved files around

--- imaging/segmentation/ct scans/step02_cnn_with_class.py
+++ imaging/segmentation/ct scans/step02_cnn_with_class.py
@@ -1,11 +1,17 @@
 from imaging.segmentation.tools.classes_cnn import *
 
 wrm = NNMealworms()
-wrm.buil_dataset()
+wrm.params["dataset_trn_ratio"] = 0.8
+wrm.params["dataset_val_ratio"] = 0.2
+wrm.params["dataset_tst_ratio"] = 0
+wrm.build_dataset()
 wrm.get_dxdydz()
 wrm.compose_transforms_trn()
 wrm.compose_transforms_val()
 wrm.compose_transforms_tst()
 wrm.cache_data()
-wrm.build_unet_model()
+wrm.build_model_unet()
 wrm.train()
+wrm.plot_loss()
+wrm.plot_metr()
+plt.show()
--- imaging/segmentation/tools/classes_cnn.py
+++ imaging/segmentation/tools/classes_cnn.py
@@ -7,25 +7,27 @@ import nibabel as nib
 import tqdm
 import datetime
 from monai.networks.nets import UNet
-from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, Spacingd, OrientationD, ScaleIntensityRanged, AsDiscreted
-from monai.data import CacheDataset, DataLoader
+from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, Spacingd, OrientationD, ScaleIntensityRanged, AsDiscreted, AsDiscrete
+from monai.inferers import sliding_window_inference
+from monai.data import CacheDataset, DataLoader, decollate_batch
 from monai.losses import DiceLoss
 from monai.metrics import DiceMetric
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+from matplotlib.animation import FuncAnimation
 from skimage.color import label2rgb
 from skimage.measure import label, regionprops
 from skimage.exposure import rescale_intensity
 
 class NNMealworms:
     def __init__(self):
-        self.params = {"dir_data_trn": 'C:\\Users\\dabe\\Desktop\\ct\\dataset_trn', # It includes images and labels
-                       "dir_data_val": 'C:\\Users\\dabe\\Desktop\\ct\\dataset_val', # It includes images and labels
-                       "experiment": datetime.datetime.now(), # datetime of the experiment
-                       "data_percentage": 0.1, # percentage of data to use
-                       "dataset_trn_ratio": 0.5, # percentage of data used for training
-                       "dataset_val_ratio": 0.5, # percentage of data used for validating
-                       "dataset_tst_ratio": 0.0, # percentage of data used for testing
+        self.params = {"dir_data_trn": 'D:\\mealworm\\ct\\dataset_trn', # It includes images and labels
+                       "dir_data_val": 'D:\\mealworm\\ct\\dataset_val', # It includes images and labels
+                       "experiment": datetime.datetime.now().strftime('%Y.%m.%d %H.%M.%S'), # datetime of the experiment
+                       "data_percentage": 1, # percentage of data to use
+                       "dataset_trn_ratio": 0.7, # percentage of data used for training
+                       "dataset_val_ratio": 0.2, # percentage of data used for validating
+                       "dataset_tst_ratio": 0.1, # percentage of data used for testing
                        'batch_trn_size': 1, # number of data per batch
                        'batch_val_size': 1, # number of data per batch
                        'batch_tst_size': 1, # number of data per batch
@@ -47,13 +49,9 @@ class NNMealworms:
                        "optimizer": 0, # 0: Adam, 1...
                        # TRAINING - VALIDATION
                        'max_iteration_trn': 100, # max iteration for training
-                       'delta_iteration_trn': 1, # number of iterations in-between validation
+                       'delta_iteration_trn': 10, # number of iterations in-between validation
                        'intensity_min': -1000, # min voxel intensity value of CT scan
                        'intensity_max': +1000, # max voxel intensity value of Ct scan
-                       'loss_function': DiceLoss(sigmoid=True), # loss function
-                       "dice_metric": DiceMetric(include_background=False, reduction="mean", get_not_nans=False), # define loss metric for the validation
-                       # PLOTTING
-                       "n_crops": 1, # if n_crops = 1 then plot xy, yz, zx planes passing through the center of the validation sample
                        }
         self.transforms_trn = None
         self.transforms_val = None
@@ -62,6 +60,12 @@ class NNMealworms:
         self.loader_val = None
         self.loader_tst = None
         self.model = None
+        self.loss_function = None
+        self.dice_metric = None
+        self.optimizer = None
+        self.epochs = np.linspace(0, self.params["max_iteration_trn"])
+        self.losses = np.zeros_like(self.epochs)
+        self.metrics = np.zeros_like(self.epochs)
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.check_gpu()
 
@@ -70,13 +74,15 @@ class NNMealworms:
         if not self.params["dataset_trn"]:
             print("Error: dataset_trn is empty. Build datasets first.")
             return
-        if not self.params["dataset_val"]:
-            print("Error: dataset_val is empty. Build datasets first.")
-            return
-        if not self.params["dataset_tst"]:
-            print("Error: dataset_tst is empty. Build datasets first.")
-            return
         self.params["dx"], self.params["dy"], self.params["dz"] = nib.load(self.params["dataset_trn"][0]["img"]).header.get_zooms()
+
+    # save whole model
+    def save_model(self, path):
+        torch.save(self.model, path)
+
+    # load whole model
+    def load_model(self, path_to_model):
+        self.model = torch.load(path_to_model)
 
     # define the transformation for the training dataset
     def compose_transforms_trn(self):
@@ -112,7 +118,7 @@ class NNMealworms:
         ])
 
     # build datasets. This function will not cache the data, just build a list of dictionaries for trn, val, and tst
-    def buil_dataset(self):
+    def build_dataset(self):
         # Each sample is a dictionary with (image path, labels path), and no actual data
         # define training and validation dataset
         path_images = sorted(glob.glob(os.path.join(self.params["dir_data_trn"], "img*.nii.gz")))
@@ -142,13 +148,13 @@ class NNMealworms:
         self.loader_tst = DataLoader(dataset_tst, batch_size=self.params["batch_tst_size"])
 
     # build a UNet model
-    def build_unet_model(self):
+    def build_model_unet(self):
         model = UNet(
             spatial_dims=3,
             in_channels=1,
             out_channels=self.params["n_classes"],
-            channels=(64, 128, 256, 512), #sequence of channels. Top block first. The length of channels should be no less than 2
-            strides=(2, 2, 2), # sequence of convolution strides. The length of stride should equal to len(channels) - 1.
+            channels=(64, 128, 256, 512, 1024), #sequence of channels. Top block first. The length of channels should be no less than 2
+            strides=(2, 2, 2, 2), # sequence of convolution strides. The length of stride should equal to len(channels) - 1.
             kernel_size=self.params["kernel_size"], #convolution kernel size, the value(s) should be odd. If sequence, its length should equal to dimensions. Defaults to 3
             up_kernel_size=self.params["up_kernel_size"], #upsampling convolution kernel size, the value(s) should be odd. If sequence, its length should equal to dimensions. Defaults to 3
             num_res_units=1,#– number of residual units. Defaults to 0.
@@ -156,6 +162,9 @@ class NNMealworms:
             dropout=self.params["dropout_ratio"]
         ).to(self.device)
         self.model = model
+        self.loss_function = DiceLoss(softmax=True)
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.params['learning_rate'], weight_decay=self.params['weight_decay'])
+        self.dice_metric = DiceMetric(include_background=False, reduction="mean")
 
     # check gpu availability and print to screen
     def check_gpu(self):
@@ -164,57 +173,81 @@ class NNMealworms:
     # train model
     def train(self):
         # define optimizer
-        if self.params["optimizer"] == 0:
-            optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.params['learning_rate'], weight_decay=self.params['weight_decay'])
         dice_score_ = -1 # initialize best_metric so that the 1st validation will result the best
-        self.model.train() # set the model to training. This has effect only on some transforms
-        #xs = np.array([random.randint(0, 200) for _ in range(self.params["n_crops"])])
-        #ys = np.array([random.randint(0, 200) for _ in range(self.params["n_crops"])])
-        #zs = np.array([random.randint(0, 200) for _ in range(self.params["n_crops"])])
         for epoch in range(self.params["max_iteration_trn"]):
+            self.model.train()  # set the model to training. This has effect only on some transforms
             epoch_loss = 0
             epoch_trn_iterator= tqdm.tqdm(self.loader_trn, desc="Training (X / X Steps) (loss=X.X)", dynamic_ncols=True, miniters=1)
             for step_trn, batch_trn in enumerate(epoch_trn_iterator):
                 # send the training data to device (GPU)
                 inputs, targets = batch_trn['img'].to(self.device), batch_trn['lbl'].to(self.device)
                 # reset the optimizer (which stores the values from the previous iteration
-                optimizer.zero_grad()
+                self.optimizer.zero_grad()
                 # forward pass
                 outputs = self.model(inputs)
-                loss = self.params["loss_function"](outputs, targets)
+                loss = self.loss_function(outputs, targets)
                 epoch_loss += loss.item()
                 # backpropagation
                 loss.backward()
                 # update metrics
-                optimizer.step()
+                self.optimizer.step()
                 # Update the progress bar description with loss and metrics
                 epoch_trn_iterator.set_description(f"Training ({epoch+1} / {self.params['max_iteration_trn']} Steps) (loss={epoch_loss:2.5f})")
-            if (epoch + 1) % self.params["delta_iteration_trn"] == 0 or (epoch + 1) == self.params["max_iteration_trn"]:
+            self.losses[epoch] = epoch_loss
+            if epoch == 0 or (epoch + 1) % self.params["delta_iteration_trn"] == 0 or (epoch + 1) == self.params["max_iteration_trn"]:
                 dice_score = self.validate(epoch)
                 # Check if this is the best model so far
                 if dice_score > dice_score_:
                     dice_score_ = dice_score
                     print(f"Model at iteration n. {epoch+1} outperforms all previous models. Saving...")
-                    torch.save(self.model.state_dict(), f"{self.params['dir_data_val']}\\exp_{self.params['experiment'].strftime('%Y.%m.%d_%H.%M.%S')}_iteration_{epoch+1:03d}_mdl.pth")
-                    self.visualize_results(epoch)
+                    torch.save(self.model, f"{self.params['dir_data_val']}\\{self.params['experiment']} iter {epoch+1:03d} mdl.pth")
+                    #self.visualize_results(epoch)
 
     # validate model
     def validate(self, epoch):
+        metric_total = 0 # set cumulative score to zero
         self.model.eval() # set the model to validation. This affects some transforms
         with torch.no_grad(): # disable gradient computation (which is useless for validation)
             epoch_val_iterator = tqdm.tqdm(self.loader_val, desc="Validate (X / X Steps) (dice=X.X)", dynamic_ncols=True, miniters=1)
-            self.params["dice_metric"].reset()
             for step_val, batch_val in enumerate(epoch_val_iterator):
                 # send the validation data to device (GPU)
                 img_val, lbl_val = batch_val["img"].to(self.device), batch_val["lbl"].to(self.device)
                 # run inference by forward passing the input data through the model
                 prd_val = self.model(img_val)
-                # update dice score
-                self.params["dice_metric"](y_pred=prd_val, y=lbl_val)
-                # Update the progress bar description with loss and metrics
-                dice_score = self.params["dice_metric"].aggregate().item()
-                epoch_val_iterator.set_description(f"Validate ({epoch+1} / {self.params['max_iteration_trn']} Steps) (dice={dice_score:2.5f})")
-            return dice_score
+                # binarize the prediction (as required by the dice metric)
+                prd_val = AsDiscrete(threshold=0.5)(prd_val)
+                self.dice_metric(y_pred=prd_val, y=lbl_val)
+                # evaluate metric
+                metric = self.dice_metric.aggregate().item()
+                metric_total += metric
+                metric_mean = metric_total / (step_val+1)
+                epoch_val_iterator.set_description(f"Validate ({epoch + 1} / {self.params['max_iteration_trn']} Steps) (dice={metric_mean:2.5f})")
+            self.metrics[epoch] = metric_mean
+            # reset the status for next validation round
+            self.dice_metric.reset()
+            return metric_mean
+
+    # plot loss vs epoch
+    def plot_loss(self, show=False):
+        plt.figure()
+        plt.plot(self.epochs, self.losses, lw=2)
+        plt.title("Training Loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        plt.tight_layout()
+        if show:
+            plt.show()
+
+    # plot metric vs epoch
+    def plot_metr(self, show=False):
+        plt.figure()
+        plt.plot(self.epochs[self.metrics != 0], self.metrics[self.metrics != 0], lw=2)
+        plt.title("Validation Metric")
+        plt.xlabel("Epoch")
+        plt.ylabel("Metric")
+        plt.tight_layout()
+        if show:
+            plt.show()
 
     # plot segmentation
     def visualize_results(self, epoch):
@@ -222,9 +255,9 @@ class NNMealworms:
             img_val, lbl_val = batch_val["img"].to(self.device), batch_val["lbl"].to(self.device)
             prd_val = self.model(img_val)
             # convert tensors from one hot encoding to single channel
-            img_array = batch_val["img"].numpy()[0, 0, :, :, :]
-            lbl_array_val = torch.argmax(batch_val["lbl"], dim=1).numpy()[0, :, :, :]
-            lbl_array_prd = torch.argmax(prd_val, dim=1).numpy()[0, :, :, :]
+            img_array = batch_val["img"].cpu().numpy()[0, 0, :, :, :]
+            lbl_array_val = torch.argmax(batch_val["lbl"], dim=1).cpu().numpy()[0, :, :, :]
+            lbl_array_prd = torch.argmax(prd_val, dim=1).cpu().numpy()[0, :, :, :]
             print(f"Plotting sample {step_val}...")
             # Find the (x,y,z) coordinates of the sample center
             x = int(np.floor(batch_val["img"].shape[2] / 2))
@@ -259,7 +292,26 @@ class NNMealworms:
             axs[0, 2].set_title(f"XY plane at Z = {z} px")
             axs[0, 2].imshow(img_label_overlay_z_val)
             axs[1, 2].imshow(img_label_overlay_z_prd)
+            # Adjust layout for better spacing
+            plt.tight_layout()
+            # Save the figure to a file
+            plt.savefig(os.path.join(self.params["dir_data_val"], f"{self.params['experiment']} iter {epoch+1:03d} img {step_val:02d} xyz {x, y, z}.png"), dpi=1200)
             #Add rectangles around regions
+            regions = regionprops(lbl_array_x_val)
+            for region in regions:
+                minr, minc, maxr, maxc = region.bbox
+                rect = Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=0.3)
+                axs[0, 0].add_patch(rect)
+            regions = regionprops(lbl_array_y_val)
+            for region in regions:
+                minr, minc, maxr, maxc = region.bbox
+                rect = Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=0.3)
+                axs[0, 1].add_patch(rect)
+            regions = regionprops(lbl_array_z_val)
+            for region in regions:
+                minr, minc, maxr, maxc = region.bbox
+                rect = Rectangle((minc, minr), maxc - minc, maxr - minr, fill=False, edgecolor='red', linewidth=0.3)
+                axs[0, 2].add_patch(rect)
             regions = regionprops(lbl_array_x_prd)
             for region in regions:
                 minr, minc, maxr, maxc = region.bbox
@@ -281,8 +333,6 @@ class NNMealworms:
                 ax.set_yticks([])
                 ax.set_xticklabels([])
                 ax.set_yticklabels([])
-            # Adjust layout for better spacing
-            plt.tight_layout()
             # Save the figure to a file
-            plt.savefig(os.path.join(self.params["dir_data_val"], f"plt_img_{step_val:02d}_iteration_{epoch+1:03d}_xyz_{x,y,z}.png"), dpi=1200)
+            plt.savefig(os.path.join(self.params["dir_data_val"], f"{self.params['experiment']} iter {epoch+1:03d} img {step_val:02d} xyz {x, y, z} box.png"), dpi=1200)
             plt.close()
