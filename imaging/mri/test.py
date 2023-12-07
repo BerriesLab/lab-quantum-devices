5cdc3a3c521d2abde145daf95947e329a4f512e6
dabeLab <mail.davide.beretta@gmail.com>
2023-12-07 17:17:40 +0100

    Add folder and moved files around

---
+++ imaging/mri/test.py
@@ -0,0 +1,23 @@
+import torch
+import torch.nn as nn
+
+# Define a simple model
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+        self.fc1 = nn.Linear(10, 5)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(5, 2)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
+
+# Create an instance of the model
+model = SimpleModel()
+
+# Access the model parameters
+for name, param in model.named_parameters():
+    print(f"Parameter name: {name}, Parameter shape: {param.shape}")
