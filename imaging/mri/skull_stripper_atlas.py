ce4c6f82c56feb3c16e404e6004d84bd756d9f93
dabeLab <mail.davide.beretta@gmail.com>
2024-01-16 17:43:34 +0100

    Add folder and moved files around

--- imaging/mri/skull_stripper_atlas.py
+++ imaging/mri/skull_stripper_atlas.py
@@ -1,20 +1,61 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import SimpleITK as sitk
+from utilities import convert_dicom_to_nifti
 
-# import database
+# Skull stripping is performed on T1, 1T1 and 1/2T1 MR images. To the purpose of model training,
+# save the pairs (skull, brain segment): the brain segment will be used to weight the registration and model training.
+
+# load canine brain atlas
+atlas = sitk.ReadImage("/Users/berri/Medical Imaging/mri/brain atlas/canine.nii.gz")
+img = sitk.GetArrayFromImage(atlas)
+# get atlas spacing
+atlas_spacing = atlas.GetSpacing()
+
+# load database
 df = pd.read_csv("/Users/berri/Medical Imaging/mri/database.csv", sep=";", index_col=False)
+# filter df: drop all rows where "modality" != MR
+df = df[df["modality"].str.lower() == "mr"]
 # filter df: drop all rows where "series description" == NaN
 df = df.dropna(subset="series description")
-# filter df: drop all rows where "series description" does not include "tra T1"
-df = df[df["series description"].str.contains("tra t1", case=False,)]
+# filter df: drop all rows where "series description" does not include "T1" and "tra"
+# this condition may be updated with a more refined case selection
+df = df[(df["series description"].str.contains("t1", case=False,)) &
+        (df["series description"].str.contains("tra", case=False,))]
+# force datetime dtype for "series datetime" -> required to measure intervals
+df["series datetime"] = pd.to_datetime(df["series datetime"], format="%Y.%m.%d %H:%M:%S")
 
-# group database
-grpd = df.groupby(["patient species",
-                   "patient id",
-                   ])
-
-for key, grp in grpd:
+# group data and find series to load in dataset
+grouped = df.groupby(["patient species", "patient id"])
+for key, grp in grouped:
+    # select dogs and make sure there are three T1 series
     if key[0] != "dog" or grp["series description"].count() != 3:
         continue
+    # measure intervals between T1 series
+    grp = grp.sort_values("series datetime").reset_index()
+    grp["interval"] = grp["series datetime"].diff()
+    # if time interval is smaller than 3 minutes, the time would not be enough for dose administration and imaging
+    # therefore the group must be discarded.
+    if grp['interval'].iloc[1:].lt(pd.Timedelta(minutes=3)).any():
+        continue
+    # add column with contrast dose
+    grp["contrast dose"] = [0, 1 / 2, 1]
+    # mark the rows as good for dataset
+    grp["dataset"] = True
     print(key)
-    print(grp[["series description", "series datetime", "image direction patient"]].sort_values("series datetime"))
+    print(grp[["series description", "series datetime", "direction", "dataset", "contrast dose"]].sort_values("series datetime"))
     print("\n")
+
+    # get nifti image from pre-contrast series T1
+    path = grp[grp["contrast dose"] == 0]["series directory"]
+    mri = sitk.ReadImage(path)
+
+
+    #convert_dicom_to_nifti(grp["series directory"],
+
+
+
+# df = df.dropna(subset="series description")[(df["modality"].str.lower() == "mr") and
+#                                             (df["series description"].str.contains("tra t1", case=False)) and
+#                                             (df["patient species"].str.lower() == "dog")]
--- imaging/mri/utilities.py
+++ imaging/mri/utilities.py
@@ -1,4 +1,5 @@
 import numpy as np
+import SimpleITK as sitk
 
 
 def cosines_to_patient(direction_cosines):
@@ -29,3 +30,44 @@ def cosines_to_patient(direction_cosines):
     orientation = ''.join(orientation_labels)
 
     return orientation
+
+
+def convert_dicom_to_nifti(path_dicom_series, path_nifti):
+    """Read a DICOM series and convert it to 3D nifti image"""
+    # Read the DICOM file
+    dicom_image = sitk.ReadImage(path_dicom_series)
+    # Save the DICOM image as a NIfTI file
+    sitk.WriteImage(dicom_image, path_nifti)
+
+
+def resample_brain_to_mri(atlas, image):
+    """ This function takes an input image (atlas) and a target image (image).
+    It calculates the resizing factor based on the original and target spacing,
+    computes the new size, and then uses sitk.Resample to perform the actual resizing. """
+    # get spacing and size of brain atlas and mr image
+    atlas_spacing = atlas.GetSpacing()
+    atlas_size = atlas.GetSize()
+    image_spacing = image.GetSpacing()
+    image_size = image.GetSize()
+    # Calculate the resizing factor
+    resizing_factor = [x / y for x, y in zip(atlas_spacing, image_spacing)]
+    # Calculate the new size
+    new_size = [int(round(x * f)) for x, f in zip(atlas_size, resizing_factor)]
+    # Create a transform
+    transform = sitk.Transform()
+    transform.SetIdentity()
+    # Set the output image spacing
+    output_spacing = image_spacing
+    output_origin = image.GetOrigin()
+    output_direction = image.GetDirection()
+
+    # Perform resampling
+    resized_image = sitk.Resample(image, new_size, transform, sitk.sitkLinear,
+                                  output_origin, output_spacing, output_direction)
+    sitk.Resample(image=atlas)
+
+    return resized_image
+
+
+# Load the original image
+original_image = sitk.ReadImage('path/to/original_image.nii.gz')
