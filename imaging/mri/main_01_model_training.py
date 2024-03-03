e30c230de9fb864e2d64763dc3606463b3d201e2
dabeLab <mail.davide.beretta@gmail.com>
2024-03-03 14:06:53 +0100

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	imaging/mri/class_BrainLearn.py

--- imaging/mri/main_01_model_training.py
+++ imaging/mri/main_01_model_training.py
@@ -5,18 +5,21 @@ bl = BrainLearn()
 bl.path_main = "E:\\gd_synthesis"
 # Set parameters
 bl.set_device("cuda")
-bl.patch_size = (64, 64, 64)
+bl.patch_size_trn = (128, 128, 128)
+bl.patch_size_val = bl.patch_size_trn
+bl.patch_size_tst = bl.patch_size_trn
 bl.patch_number = 20
 bl.batch_trn_size = 1
 bl.batch_tst_size = 1
 bl.batch_val_size = 1
 bl.max_iteration_trn = 1000
 bl.delta_iteration_trn = 50
+bl.batch_trn_size = 1
 # Build dataset
 bl.data_percentage = 1
 bl.dataset_trn_ratio = 0.7
-bl.dataset_val_ratio = 0.2
-bl.dataset_tst_ratio = 0.1
+bl.dataset_val_ratio = 0.3
+bl.dataset_tst_ratio = 0.0
 bl.build_dataset(shuffle=False)
 # Compose transformation - Edit the class method to amend
 bl.compose_transforms_trn()
@@ -33,11 +36,13 @@ bl.set_metric_function()
 bl.set_optimizer()
 # Train model
 bl.train()
-# Save Loss and Score
+# Save results
 bl.save_dataset_trn_paths_to_csv()
 bl.save_dataset_val_paths_to_csv()
 bl.save_dataset_tst_paths_to_csv()
 bl.save_model_attributes()
 bl.save_loss()
 bl.save_score()
+# Test model
+# bl.test()
 
